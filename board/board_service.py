f274646855657c01b205d1d988da545c4fa23daf
valentin1235 <valentin123@naver.com>
2020-04-29 00:23:35 +0900

    [게시판 수정] - path 파라미터로 게시판 아이디를 받음 - flask validator를 통해서 수정하고자 하는 게시판 이름 유효성검사 - 마스터권한이 아니면 400리턴 - 수정하고자 하는 게시판이 이미 지워졌으면 404리턴
    [게시판 삭제]
    - soft delete
    - path 파라미터리 게시판 아이디를 받음
    - body로 게시판 삭제 True를 받음, 삭제 API이기 때문에 True이외의 액션은 400처리
    - 마스터권한이 아니면 400리턴
    - 삭제하고자 하는 게시판이 이미 삭제되었으면 400리턴
    - 게시판을 삭제하고 해당 게시판에 포함된 모든 게시물도 삭제함
    
    [게시물 생성]
    - path 파라미터로 게시판 아이디를 받고 해당 게시판에 게시물 생성
    - flask validator를 통해서 게시물 제목, 게시물 내용 유효성 검사
    - 이미 삭제된 게시판에 게시물을 생성하려는 경우 404리턴
    
    [게시물 리스트 표출]
    - path 파라미터로 게시판 아이디를 받고 해당 게시판의 게시물 리스트 표출
    - 이미 삭제된 게시판의 게시물 리스트를 가져오려는 경우 404리턴
    - 삭제되지 않은(is_deleted==false) 게시물을 리스트 형식으로 최근 생성된 순으로 정렬해서 가져옴
    
    [게시물 상세 정보 표출]
    - path파라미터로 게시판아이디와 게시물 아이디를 같이 받아서 게시물 상세정보 표출
    - 이미 삭제된 게시판 또는 게시물의 정보를 가져오려는 경우 404 리턴
    
    [게시물 수정]
    - path 파라미터로 게시판 아이디와 게시물 아이디를 받아서 해당 게시물의 내용을 수정
    - flask validator를 통해서 게시물 제목과 게시물 내용의 유효성검사
    - 마스터권한이 아니거나, 게시물의 생성자가 게시물의 수정하려는 경우가 아닌 경우 403리턴
    - 이미 삭제된 게시판 또는 게시물을 수정하려는 경우 404 리턴
    - 게시물 제목 또는 게시물 내용을 업데이트 하고, 게시물 수정자를 업데이트함.
    
    [게시물 삭제]
    - soft delete
    - path 파라미터로 게시판 아이디와 게시물 아이디를 받고, body로 삭제여부 True를 받아서 해당 게시물의 내용을 삭제
    - 삭제여부가 True가 아닌경우 400리턴
    - 마스터권한이 아니거나, 게시물의 생성자가 게시물의 삭제하려는 경우가 아닌 경우 403리턴
    - 이미 삭제된 게시판 또는 게시물을 삭제하려는 경우 404 리턴
    - is_deleted를 True로 바꿔주고 수정자 업데이트

--- board/board_service.py
+++ board/board_service.py
@@ -1,5 +1,6 @@
 from flask import jsonify, g
-from .board_dao import engine, Board
+from .board_dao import engine, Board, Article
+from user.user_dao import User
 from sqlalchemy import exists
 from sqlalchemy.orm import sessionmaker
 
@@ -10,9 +11,6 @@ class BoardService:
         try:
             Session = sessionmaker(bind=engine)
             session = Session()
-
-            if session.query(exists().where(Board.name == board_info['name'])).one()[0]:
-                return jsonify({'message': 'DUPLICATE_NAME'}), 400
 
             board.uploader = board_info['uploader']
             board.name = board_info['name']
@@ -62,9 +60,8 @@ class BoardService:
         try:
             Session = sessionmaker(bind=engine)
             session = Session()
-
-            if session.query(exists().where(Board.name == board_info['new_name'])).one()[0]:
-                return jsonify({'message': 'DUPLICATE_NAME'}), 400
+            if session.query(Board.is_deleted).filter(Board.id == board_info['board_id']).one()[0]:
+                return  jsonify({'message': 'BOARD_NOT_EXISTS'}), 404
 
             (session
              .query(Board)
@@ -91,9 +88,17 @@ class BoardService:
             Session = sessionmaker(bind=engine)
             session = Session()
 
+            if session.query(Board.is_deleted).filter(Board.id == board_info['board_id']).one()[0]:
+                return  jsonify({'message': 'BOARD_NOT_EXISTS'}), 404
+
             (session
              .query(Board)
              .filter(Board.id == board_info['board_id'])
+             .update({'is_deleted': board_info['is_deleted'], 'modifier': board_info['modifier']}))
+
+            (session
+             .query(Article)
+             .filter(Article.board_id == board_info['board_id'])
              .update({'is_deleted': board_info['is_deleted'], 'modifier': board_info['modifier']}))
 
             session.commit()
@@ -108,5 +113,179 @@ class BoardService:
                 except Exception as e:
                     print(e)
                     return jsonify({'message': 'SESSION_CLOSE_ERROR'}), 500
+
+        return jsonify({'message': 'SUCCESS'}), 200
+
+    def make_article(self, article_info):
+        article = Article()
+        try:
+            Session = sessionmaker(bind=engine)
+            session = Session()
+
+            if session.query(Board.is_deleted).filter(Board.id == article_info['board_id']).one()[0]:
+                return  jsonify({'message': 'BOARD_NOT_EXISTS'}), 404
+
+            article.board_id = article_info['board_id']
+            article.uploader = article_info['uploader']
+            article.title = article_info['title']
+            article.content = article_info['content']
+            session.add(article)
+            session.commit()
+
+        except Exception as e:
+            print(e)
+            return jsonify({'message': e}), 500
+
+        finally:
+                try:
+                    session.close()
+                except Exception as e:
+                    print(e)
+                    return jsonify({'message': 'SESSION_CLOSE_ERROR'}), 500
+
+        return jsonify({'message': 'SUCCESS'}), 200
+
+    def get_article_list(self, board_id):
+        try:
+            Session = sessionmaker(bind=engine)
+            session = Session()
+
+            # article_list = relationship("Writer", foreign_keys='Article.writer_id')
+            if session.query(Board.is_deleted).filter(Board.id == board_id).one()[0]:
+                return  jsonify({'message': 'BOARD_NOT_EXISTS'}), 404
+
+            article_list = (session
+                            .query(Article.id, Article.title, Article.create_at, User.full_name)
+                            .join(User, User.id == Article.uploader)
+                            .filter(Article.board_id == board_id, Article.is_deleted == False)
+                            .order_by(Article.create_at.asc())
+                            .all())
+
+            articles = [{
+                    'id': article[0],
+                    'title': article[1],
+                    'created_at': article[2],
+                    'author': article[3]
+                } for article in article_list]
+
+        except Exception as e:
+            print(e)
+            return jsonify({'message': e}), 500
+
+        finally:
+            try:
+                session.close()
+            except Exception as e:
+                print(e)
+                return jsonify({'message': 'SESSION_CLOSE_ERROR'}), 500
+
+        return jsonify({'boards': articles}), 200
+
+    def get_article_detail(self, article_info):
+        try:
+            Session = sessionmaker(bind=engine)
+            session = Session()
+
+            # article_list = relationship("Writer", foreign_keys='Article.writer_id')
+            if session.query(Board.is_deleted).filter(Board.id == article_info['board_id']).one()[0]:
+                return  jsonify({'message': 'BOARD_NOT_EXISTS'}), 404
+            if session.query(Article.is_deleted).filter(Article.id == article_info['article_id']).one()[0]:
+                return  jsonify({'message': 'ARTICLE_NOT_EXISTS'}), 404
+
+            article_detail = (session
+                .query(Article.title, Article.content, Article.create_at, Article.updated_at, User.full_name)
+                .join(User, User.id == Article.uploader)
+                .filter(Article.id == article_info['article_id'], Article.board_id == article_info['board_id'], Article.is_deleted == False)
+                .one())
+
+            article_detail = {
+                'title': article_detail[0],
+                'content': article_detail[1],
+                'author': article_detail[4],
+                'created_at': article_detail[2],
+                'updated_at': article_detail[3],
+            }
+
+        except Exception as e:
+            print(e)
+            return jsonify({'message': e}), 500
+
+        finally:
+            try:
+                session.close()
+            except Exception as e:
+                print(e)
+                return jsonify({'message': 'SESSION_CLOSE_ERROR'}), 500
+
+        return jsonify({'boards': article_detail}), 200
+
+    def edit_article(self, article_info):
+        try:
+            Session = sessionmaker(bind=engine)
+            session = Session()
+            uploader_db = session.query(Article.uploader).filter(Article.id == article_info['article_id']).one()[0]
+            if (uploader_db != article_info['modifier']) and (article_info['auth_type_id'] != 1):
+                return jsonify({'message': 'UNAUTHORIZED_ACTION'}), 403
+
+            if session.query(Board.is_deleted).filter(Board.id == article_info['board_id']).one()[0]:
+                return jsonify({'message': 'BOARD_NOT_EXISTS'}), 404
+
+            if session.query(Article.is_deleted).filter(Article.id == article_info['article_id']).one()[0]:
+                return  jsonify({'message': 'ARTICLE_NOT_EXISTS'}), 404
+
+            (session
+             .query(Article)
+             .filter(Article.board_id == article_info['board_id'], Article.id == article_info['article_id'])
+             .update({'title': article_info['new_title'], 'content': article_info['new_content'], 'modifier': article_info['modifier']}))
+
+            session.commit()
+
+        except Exception as e:
+            print(e)
+            return jsonify({'message': e}), 500
+
+        finally:
+            try:
+                session.close()
+            except Exception as e:
+                print(e)
+                return jsonify({'message': 'SESSION_CLOSE_ERROR'}), 500
+
+        return jsonify({'message': 'SUCCESS'}), 200
+
+    def delete_article(self, article_info):
+        article = Article()
+        try:
+            Session = sessionmaker(bind=engine)
+            session = Session()
+            print(article_info)
+            uploader_db = session.query(Article.uploader).filter(Article.id == article_info['article_id']).one()[0]
+            print(uploader_db)
+            if (uploader_db != article_info['modifier']) and (article_info['auth_type_id'] != 1):
+                return jsonify({'message': 'UNAUTHORIZED_ACTION'}), 403
+
+            if session.query(Board.is_deleted).filter(Board.id == article_info['board_id']).one()[0]:
+                return jsonify({'message': 'BOARD_NOT_EXISTS'}), 404
+
+            if session.query(Article.is_deleted).filter(Article.id == article_info['article_id']).one()[0]:
+                return  jsonify({'message': 'ARTICLE_NOT_EXISTS'}), 404
+
+            (session
+             .query(Article)
+             .filter(Article.id == article_info['article_id'])
+             .update({'is_deleted': article_info['is_deleted'], 'modifier': article_info['modifier']}))
+
+            session.commit()
+
+        except Exception as e:
+            print(e)
+            return jsonify({'message': e}), 500
+
+        finally:
+            try:
+                session.close()
+            except Exception as e:
+                print(e)
+                return jsonify({'message': 'SESSION_CLOSE_ERROR'}), 500
 
         return jsonify({'message': 'SUCCESS'}), 200
--- board/board_view.py
+++ board/board_view.py
@@ -26,7 +26,6 @@ class BoardView:
               rules=[MaxLength(20)])
     )
     def make_board(*args):
-        print(args)
         user_info = g.user_info
         if user_info.get('auth_type_id') != 1:
             return jsonify({'message': 'UNAUTHORIZED_ACTION'}), 400
@@ -35,7 +34,6 @@ class BoardView:
             'uploader': user_info['user_id'],
             'name': args[0]
         }
-        print(board_info)
         board_service = BoardService()
         result = board_service.make_board(board_info)
         return result
@@ -75,7 +73,6 @@ class BoardView:
         Param('is_deleted', JSON, int)
     )
     def delete_board(*args):
-        print(args)
         user_info = g.user_info
         if user_info.get('auth_type_id') != 1:
             return jsonify({'message': 'UNAUTHORIZED_ACTION'}), 400
@@ -89,7 +86,106 @@ class BoardView:
         else:
             return jsonify({'message': 'INVALID_ACTION'}), 400
 
-        print(board_info)
         board_service = BoardService()
         result = board_service.delete_board(board_info)
+        return result
+
+    @board_app.route("/<int:board_id>", methods=["POST"], endpoint='make_article')
+    @login_required
+    @validate_params(
+        Param('board_id', PATH, int),
+        Param('title', JSON, str,
+              rules=[MaxLength(200)]),
+        Param('content', JSON, str,
+              rules=[MaxLength(10000)])
+    )
+    def make_article(*args):
+        user_info = g.user_info
+        # if user_info.get('auth_type_id') != 1:
+        #     return jsonify({'message': 'UNAUTHORIZED_ACTION'}), 400
+
+        article_info = {
+            'board_id': args[0],
+            'uploader': user_info['user_id'],
+            'title': args[1],
+            'content': args[2]
+        }
+        board_service = BoardService()
+        result = board_service.make_article(article_info)
+        return result
+
+    @board_app.route("/<int:board_id>", methods=["GET"], endpoint='get_article_list')
+    @login_required
+    @validate_params(
+        Param('board_id', PATH, int)
+    )
+    def get_article_list(*args):
+        board_id = args[0]
+        board_service = BoardService()
+        result = board_service.get_article_list(board_id)
+        return result
+
+    @board_app.route("/<int:board_id>/<int:article_id>", methods=["GET"], endpoint='get_article_detail')
+    @login_required
+    @validate_params(
+        Param('board_id', PATH, int),
+        Param('article_id', PATH, int)
+    )
+    def get_article_detail(*args):
+        article_info = {
+            'board_id': args[0],
+            'article_id': args[1]
+        }
+        board_service = BoardService()
+        result = board_service.get_article_detail(article_info)
+        return result
+
+    @board_app.route("/<int:board_id>/<int:article_id>", methods=["PUT"], endpoint='edit_article')
+    @login_required
+    @validate_params(
+        Param('board_id', PATH, int),
+        Param('article_id', PATH, int),
+        Param('title', JSON, str,
+              rules=[MaxLength(200)]),
+        Param('content', JSON, str,
+              rules=[MaxLength(10000)])
+    )
+    def edit_article(*args):
+        user_info = g.user_info
+        article_info = {
+            'board_id': args[0],
+            'article_id': args[1],
+            'new_title': args[2],
+            'new_content': args[3],
+            'modifier': user_info['user_id'],
+            'auth_type_id': user_info['auth_type_id']
+        }
+        board_service = BoardService()
+        result = board_service.edit_article(article_info)
+
+        return result
+
+    @board_app.route("/<int:board_id>/<int:article_id>", methods=["DELETE"], endpoint='delete_article')
+    @login_required
+    @validate_params(
+        Param('board_id', PATH, int),
+        Param('article_id', PATH, int),
+        Param('is_deleted', JSON, int)
+    )
+    def delete_article(*args):
+        user_info = g.user_info
+        article_info = {
+            'board_id': args[0],
+            'article_id': args[1],
+            'modifier': user_info['user_id'],
+            'auth_type_id': user_info['auth_type_id']
+        }
+        if args[2] == 1:
+            article_info['is_deleted'] = True
+        else:
+            return jsonify({'message': 'INVALID_ACTION'}), 400
+
+        board_service = BoardService()
+        result = board_service.delete_article(article_info)
+
         return result
